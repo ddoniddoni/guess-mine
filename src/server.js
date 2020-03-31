0f3bb4da39f8e651a153b3c705302c4782169ceb
ddoniddoni <psdkei@naver.com>
2020-03-31 18:04:16 +0900

    Setting chatApp

--- src/server.js
+++ src/server.js
@@ -20,5 +20,10 @@ const server = app.listen(PORT, handleListening);
 const io = socketIO.listen(server);
 
 io.on("connection", socket => {
-  setTimeout(() => socket.broadcast.emit("hello"), 5000);
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", { message, nickname: socket.nickname || "Anon" });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
--- src/static/index.js
+++ src/static/index.js
@@ -1,5 +1,17 @@
 const socket = io("/");
 
-socket.on("hello", () => console.log("Somebody Joined"));
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You : ${message}`);
+}
 
-socket.emit("hello guys");
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
