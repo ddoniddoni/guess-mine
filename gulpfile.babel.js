6f21d5abba5264420bf90b759198b3b0cf257bb5
ddoniddoni <psdkei@naver.com>
2020-04-01 17:07:04 +0900

    Add Gulp autoprefixer and csso

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,37 @@
 import gulp from "gulp";
 import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+
+sass.compiler = require("node-sass");
+
 const paths = {
   styles: {
     src: "assets/scss/styles.scss",
-    dest: "src/static/styles"
+
+    dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss"
   }
 };
 
-export function styles() {
-  return gulp.src(paths.styles.src).pipe(sass()).pipe(gulp.dest(paths.styles.dest));
+function styles() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslists: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles() {
+  gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
