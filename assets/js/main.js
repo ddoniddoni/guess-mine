512ba2c4a03d08eb7de12cb5022da0c75f77ebda
ddoniddoni <psdkei@naver.com>
2020-04-01 17:48:19 +0900

    Add gulp-Browserify

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,12 @@
+import { handleMessageNotif } from "./chat";
 const socket = io("/");
 
 function sendMessage(message) {
   socket.emit("newMessage", { message });
-  console.log(`You : ${message}`);
+  console.log(`You: ${message}`);
 }
-
 function setNickname(nickname) {
   socket.emit("setNickname", { nickname });
-}
-
-function handleMessageNotif(data) {
-  const { message, nickname } = data;
-  console.log(`${nickname}: ${message}`);
 }
 
 socket.on("messageNotif", handleMessageNotif);
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,8 @@ import gulp from "gulp";
 import sass from "gulp-sass";
 import autoprefixer from "gulp-autoprefixer";
 import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
 
 sass.compiler = require("node-sass");
 
@@ -11,11 +13,18 @@ const paths = {
 
     dest: "src/static/styles",
     watch: "assets/scss/**/*.scss"
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js"
   }
 };
 
-function styles() {
-  return gulp
+const clean = () => del(["src/static"]);
+
+const styles = () =>
+  gulp
     .src(paths.styles.src)
     .pipe(sass())
     .pipe(
@@ -26,12 +35,13 @@ function styles() {
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
-}
 
-function watchFiles() {
+const js = () => gulp.src(paths.js.src).pipe(bro()).pipe(gulp.dest(paths.js.dest));
+
+const watchFiles = () => {
   gulp.watch(paths.styles.watch, styles);
-}
-
-const dev = gulp.series([styles, watchFiles]);
+  gulp.watch(paths.js.watch, js);
+};
+const dev = gulp.series(clean, styles, js, watchFiles);
 
 export default dev;
