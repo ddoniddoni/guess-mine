39cfd11beb6ccd631c870aa7f4a32a8685be34bb
ddoniddoni <psdkei@naver.com>
2020-04-03 17:12:33 +0900

    Add Notification connect and disconnect

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,5 +1,16 @@
 const notifications = document.getElementById("jsNotifications");
 
+const fireNotification = (text, color) => {
+  const notification = document.createElement("div");
+  notification.innerText = text;
+  notification.style.backgroundColor = color;
+  notifications.appendChild(notification);
+};
+
 export const handleNewUser = ({ nickname }) => {
-  console.log(nickname, "just joined");
+  fireNotification(`${nickname} just joined!`, "rgb(0, 122, 255)");
+};
+
+export const handleDisconnected = ({ nickname }) => {
+  fireNotification(`${nickname} just left`, "rgb(255,149,0)");
 };
--- src/socketController.js
+++ src/socketController.js
@@ -1,9 +1,13 @@
 import events from "./events";
 
 const socketController = socket => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
   socket.on(events.setNickname, ({ nickname }) => {
-    socket.broadcast.emit(events.newUser, { nickname });
+    broadcast(events.newUser, { nickname });
     socket.nickname = nickname;
+  });
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
   });
 };
 
