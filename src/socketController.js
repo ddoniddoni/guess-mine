585258b6866f061861608467faf1a12983e6a3cf
ddoniddoni <psdkei@naver.com>
2020-04-04 17:31:25 +0900

    Add Realtime Painting

--- src/socketController.js
+++ src/socketController.js
@@ -6,12 +6,17 @@ const socketController = socket => {
     broadcast(events.newUser, { nickname });
     socket.nickname = nickname;
   });
-  socket.on(events.disconnect, () => {
-    broadcast(events.disconnected, { nickname: socket.nickname });
-  });
+  socket.on(events.disconnect, () => broadcast(events.disconnected, { nickname: socket.nickname }));
 
   socket.on(events.sendMsg, ({ message }) => {
     broadcast(events.newMsg, { message, nickname: socket.nickname });
+  });
+
+  socket.on(events.beginPath, ({ x, y }) => broadcast(events.beganPath, { x, y }));
+
+  socket.on(events.strokePath, ({ x, y }) => {
+    broadcast(events.strokedPath, { x, y });
+    console.log(x, y);
   });
 };
 
